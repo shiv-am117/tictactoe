6743a66c303765efd691d41474ad91464666feb7
shiv-am117 <goel.shivam117@gmail.com>
2018-12-21 12:13:55 +0530

    Updates

---
+++ precache-manifest.fe4183fd5eed46584d9e927acea4a781.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "d662422d362fabed5bd6",
+    "url": "/myapp/static/js/main.d662422d.chunk.js"
+  },
+  {
+    "revision": "6bd1ea98e814ca143deb",
+    "url": "/myapp/static/js/1.6bd1ea98.chunk.js"
+  },
+  {
+    "revision": "d662422d362fabed5bd6",
+    "url": "/myapp/static/css/main.3597f909.chunk.css"
+  },
+  {
+    "revision": "9d74790475dae37178c1b487fb485d6c",
+    "url": "/myapp/index.html"
+  }
+];
